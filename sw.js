57248c96687c18d98aedafb085788c16ece881c3
yourmentorlyofficial-png <your.mentorly.official@gmail.com>
2026-08-25 23:41:45 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(fetch(e.request).catch(() => new Response('Offline')));
+});
+
