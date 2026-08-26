9f9b37cbd4657283020df164cfbd2be6b4d5315b
yourmentorlyofficial-png <your.mentorly.official@gmail.com>
2026-08-26 09:47:26 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,96 +1,39 @@
-const CACHE_NAME = "dabsy-v1";
-
-const APP_FILES = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.webmanifest",
-  "./icon-192.svg",
-  "./icon-512.svg"
+const CACHE_NAME = 'dabsy-v1-cache';
+const ASSETS = [
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json'
 ];
 
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
 
-/* INSTALL */
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
 
-self.addEventListener(
-  "install",
-  event => {
-
-    event.waitUntil(
-
-      caches
-        .open(CACHE_NAME)
-        .then(cache =>
-          cache.addAll(APP_FILES)
-        )
-
-    );
-
-    self.skipWaiting();
-
-  }
-);
-
-
-/* ACTIVATE */
-
-self.addEventListener(
-  "activate",
-  event => {
-
-    event.waitUntil(
-
-      caches
-        .keys()
-        .then(keys =>
-
-          Promise.all(
-
-            keys
-              .filter(
-                key =>
-                  key !== CACHE_NAME
-              )
-              .map(key =>
-                caches.delete(key)
-              )
-
-          )
-
-        )
-
-    );
-
-    self.clients.claim();
-
-  }
-);
-
-
-/* FETCH */
-
-self.addEventListener(
-  "fetch",
-  event => {
-
-    event.respondWith(
-
-      caches
-        .match(event.request)
-        .then(cached => {
-
-          return (
-            cached ||
-            fetch(event.request)
-          );
-
-        })
-        .catch(() =>
-          caches.match("./index.html")
-        )
-
-    );
-
-  }
-);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request).catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('./index.html');
+        }
+      });
+    })
+  );
+});
